2057aea64951964ef350223633f5cb110606f942
MerabiRizhamadze <mrizh18@freeuni.edu.ge>
2024-06-20 03:13:29 +0400

    Adding Tests: MultipleChoiceQuestions basic tests (blackbox)

--- quizz_site/src/main/java/DAO/test.java
+++ quizz_site/src/main/java/DAO/test.java
@@ -1,4 +1,71 @@
 package DAO;
 
+import database.DBConnection;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import questions.MultipleChoiceQuestion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 public class test {
+    static Connection connection;
+    static PreparedStatement statement;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        connection = DBConnection.getConnection();
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        statement.close();
+        connection.close();
+    }
+
+    @Test
+    public void testDBConnectionAcquired() throws SQLException, ClassNotFoundException {
+        assertNotNull(connection);
+    }
+
+    @Test
+    public void testMultipleChoiceQuestion_addQuestion() throws SQLException, ClassNotFoundException {
+        statement = connection.prepareStatement("select count(*) from MultipleChoiceQuestions");
+        ResultSet initResultSet = statement.executeQuery();
+        initResultSet.next();
+        int initialRows = initResultSet.getInt(1);
+
+        MultipleChoiceQuestionDAO questionDAO = new MultipleChoiceQuestionDAO();
+        questionDAO.addQuestion(new MultipleChoiceQuestion());
+
+        ResultSet finalResultSet = statement.executeQuery();
+        finalResultSet.next();
+        int finalRows = finalResultSet.getInt(1);
+
+        assertEquals(initialRows + 1, finalRows);
+    }
+
+    @Test
+    public void testMultipleChoiceQuestion_getQuestion() throws SQLException, ClassNotFoundException {
+        statement = connection.prepareStatement("select * from MultipleChoiceQuestions LEFT JOIN databaza.MultipleChoiceQuestionsWrongAnswers MCQWA on MultipleChoiceQuestions.id = MCQWA.questionId");
+        ResultSet resultSet = statement.executeQuery();
+        while(resultSet.next()){
+            System.out.println(resultSet.getInt("id"));
+            System.out.println(resultSet.getString("question"));
+            System.out.println(resultSet.getString("correctAnswer"));
+
+            MultipleChoiceQuestionDAO mcqDAO = new MultipleChoiceQuestionDAO();
+            MultipleChoiceQuestion mcq = mcqDAO.getQuestion();
+
+            assertEquals(resultSet.getInt("id"), mcq.getQuestionId());
+            assertEquals(resultSet.getString("question"), mcq.getQuestion());
+            assertEquals(resultSet.getString("correctAnswer"), mcq.getCorrectAnswer());
+        }
+    }
 }
--- quizz_site/src/main/java/questions/test.java
+++ quizz_site/src/main/java/questions/test.java
@@ -1,4 +1,45 @@
 package questions;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class test {
+
+    @Test
+    public void testMultipleChoice_bean() {
+        int id = -1;
+
+        String question = "What is next: 1 2 4 8 16 ?";
+        List<String> incorrectAnswers = new ArrayList<>();
+        incorrectAnswers.add("64");
+        incorrectAnswers.add("0");
+        incorrectAnswers.add("16");
+        String correctAnswer = "31";
+
+        MultipleChoiceQuestion mcq = new MultipleChoiceQuestion();
+
+        assertNull(mcq.getQuestion());
+        assertNull(mcq.getCorrectAnswer());
+        assertNull(mcq.getIncorrectAnswers());
+
+        mcq.setQuestionId(id);
+        mcq.setQuestion(question);
+        mcq.setCorrectAnswer(correctAnswer);
+        mcq.setIncorrectAnswers(incorrectAnswers);
+
+        assertNotNull(mcq.getQuestion());
+        assertNotNull(mcq.getCorrectAnswer());
+        assertNotNull(mcq.getIncorrectAnswers());
+
+        System.out.println("id: " + mcq.getQuestionId());
+        System.out.println("question: " + mcq.getQuestion());
+        System.out.println("correct answer: " + mcq.getCorrectAnswer());
+        System.out.println("incorrect answers: " + mcq.getIncorrectAnswers());
+        System.out.println(mcq);
+    }
 }
