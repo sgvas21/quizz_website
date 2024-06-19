f70e86cb7c8e85af19b9bf5045d64fd022eeb4f5
MerabiRizhamadze <mrizh18@freeuni.edu.ge>
2024-06-20 03:19:01 +0400

    Added Implementation: Question Interface. MultipleChoiceQuestion bean class

---
+++ quizz_site/src/main/java/questions/MultipleChoiceQuestion.java
@@ -0,0 +1,59 @@
+package questions;
+
+import javax.sql.rowset.CachedRowSet;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class MultipleChoiceQuestion implements Question {
+    private int id;
+    private String question;
+    private List<String> incorrectAnswers;
+    private String correctAnswer;
+
+    public MultipleChoiceQuestion() {
+    }
+
+    public int getQuestionId() {
+        return id;
+    }
+
+    public void setQuestionId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(List<String> answer) {
+        this.incorrectAnswers = answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "MultipleQuestion {" +
+                "id=" + this.id +
+                ", question='" + this.question + '\'' +
+                ", correct answer='" + this.correctAnswer + '\'' +
+                ", incorrect answers=" + this.incorrectAnswers.toString() +
+                '}';
+    }
+}
---
+++ quizz_site/src/main/java/questions/Question.java
@@ -0,0 +1,10 @@
+package questions;
+
+public interface Question {
+    int getQuestionId();
+    void setQuestionId(int questionId);
+
+    String getQuestion();
+    void setQuestion(String question);
+
+}
